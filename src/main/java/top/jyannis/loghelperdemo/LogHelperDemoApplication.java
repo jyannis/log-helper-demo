078bbea8d86d5aebc2d73d8badb5173c5d4bca33
jyannis <123400197@qq.com>
2021-05-29 21:12:21 +0800

    default mode

---
+++ src/main/java/top/jyannis/loghelperdemo/LogHelperDemoApplication.java
@@ -0,0 +1,13 @@
+package top.jyannis.loghelperdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogHelperDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogHelperDemoApplication.class, args);
+	}
+
+}
